1d9f9d7418a5e849ab474ffaacb3e17c61687ab1
daniel <dango991@gmail.com>
2025-05-12 20:42:25 +0300

    Implemented mitigation techniques

--- app/app.js
+++ app/app.js
@@ -3,17 +3,34 @@ const session = require('express-session');
 const passport = require('passport');
 const SamlStrategy = require('passport-saml').Strategy;
 const bodyParser = require('body-parser');
+const sanitize = require('express-xss-sanitizer');
+const lusca = require('lusca');
 const fs = require('fs');
-
 const app = express();
 const PORT = 3000;
 
+app.set('trust proxy', 1);
+
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(session({
-  secret: '',
+  name: 'Session_ID',
+  secret: '{secret}',
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
+  cookie: {
+    sameSite: 'lax',
+    maxAge: 28800000,
+    secure: true,
+    httpOnly: true
+  }
 }));
+
+app.use((req, res, next) => { 
+  res.setHeader('Strict-Transport-Security', 'max-age=63072000');
+  res.setHeader('Content-Security-Policy', "default-src 'self'; frame-ancestors 'self'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; base-uri 'self'; form-action 'self';");
+next(); 
+});
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -21,11 +38,14 @@ app.use(passport.session());
 passport.use(new SamlStrategy(
   {
     path: '/login/callback',
-    entryPoint: '',
-    issuer: 'http://localhost:3000/',
-    cert: ''
+    entryPoint: '{entryPoint}',
+    issuer: '{issuer}', 
+    cert: fs.readFileSync('{okta_cert_file}', 'utf-8'),
+    decryptionPvk: fs.readFileSync('{pvk_file}', 'utf-8'),
+    acceptedClockSkewMs: -240000,
+    validateInResponseTo: true
   },
-  function (profile, done) {
+  (profile, done) => {
     return done(null, profile);
   }
 ));
@@ -33,10 +53,23 @@ passport.use(new SamlStrategy(
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
 
+const CSRFSetting = (req, res, next) => { 
+  if (req.method === 'POST' && req.path === '/login/callback') { 
+    return next(); 
+  } 
+  return lusca.csrf()(req, res, next); 
+} 
+
+app.use(CSRFSetting); 
+
 app.get('/', (req, res) => {
   if (req.isAuthenticated()) {
-    res.send(`<h1>Hello, ${req.user.nameID}</h1><a href="/logout"><button>Logout</button></a>`);
+    req.user.nameID = sanitize(req.user.nameID);
+    req.user.department = sanitize(req.user.department);
+    res.status(200);
+    res.send(`<h1>Hello, ${req.user.nameID}! Your department is: ${req.user.department}</h1><form action="/logout" method="POST"><input type='hidden' name='_csrf' value='${req.csrfToken()}'><button>Logout</button></form>`);
   } else {
+    res.status(200);
     res.send('<a href="/login"><button>Login with SAML</button></a>');
   }
 });
@@ -47,21 +80,32 @@ app.get('/login',
   })
 );
 
+app.get('/login/callback', (req, res) => {
+    res.status(401);
+    res.send('Unauthorized');
+  }
+);
+
 app.post('/login/callback',
   passport.authenticate('saml', {
     failureRedirect: '/',
   }),
   (req, res) => {
     res.redirect('/');
-  }
+    }
 );
 
-app.get('/logout', (req, res) => {
+app.post('/logout', (req, res) => {
   req.logout(() => {
     res.redirect('/');
   });
 });
 
+app.use((req, res) => { 
+  res.status(404);
+  res.send('Not found'); 
+}); 
+
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
